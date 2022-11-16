0aecfe379e232452f16563bd81c036dc77cb18ca
Reed Cowan <reed.cowan@Reeds-MacBook-Pro-2.local>
2022-11-16 18:28:39 -0500

    integrated utils lib

--- oauth/oauth.go
+++ oauth/oauth.go
@@ -1,14 +1,15 @@
 package oauth
 
 import (
-	"bookstore_oauth-go/oauth/errors"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"strconv"
 	"strings"
 	"time"
 
+	"github.com/cowanrc/bookstore_utils-go/rest_errors"
 	"github.com/mercadolibre/golang-restclient/rest"
 )
 
@@ -20,24 +21,23 @@ const (
 	paramAccessToken = "access_token"
 )
 
+var (
+	oauthRestClient = rest.RequestBuilder{
+		BaseURL: "http://localhost:8080",
+		Timeout: 200 * time.Millisecond,
+	}
+)
+
 type accessToken struct {
 	Id       string `json:"id"`
 	UserId   int64  `json:"user_id"`
 	ClientId int64  `json:"client_id"`
 }
 
-var (
-	oauthRestClient = rest.RequestBuilder{
-		BaseURL: "http://localhost:8080",
-		Timeout: 200 * time.Microsecond,
-	}
-)
-
 func IsPublic(request *http.Request) bool {
 	if request == nil {
 		return true
 	}
-
 	return request.Header.Get(headerXPublic) == "true"
 }
 
@@ -45,12 +45,10 @@ func GetCallerId(request *http.Request) int64 {
 	if request == nil {
 		return 0
 	}
-
 	callerId, err := strconv.ParseInt(request.Header.Get(headerXCallerId), 10, 64)
 	if err != nil {
 		return 0
 	}
-
 	return callerId
 }
 
@@ -58,16 +56,14 @@ func GetClientId(request *http.Request) int64 {
 	if request == nil {
 		return 0
 	}
-
 	clientId, err := strconv.ParseInt(request.Header.Get(headerXClientId), 10, 64)
 	if err != nil {
 		return 0
 	}
-
 	return clientId
 }
 
-func AuthenticateRequest(request *http.Request) *errors.RestErr {
+func AuthenticateRequest(request *http.Request) rest_errors.RestErr {
 	if request == nil {
 		return nil
 	}
@@ -81,15 +77,13 @@ func AuthenticateRequest(request *http.Request) *errors.RestErr {
 
 	at, err := getAccessToken(accessTokenId)
 	if err != nil {
-		if err.Status == http.StatusNotFound {
+		if err.Status() == http.StatusNotFound {
 			return nil
 		}
 		return err
 	}
-
-	request.Header.Add(headerXCallerId, fmt.Sprintf("%v", at.UserId))
 	request.Header.Add(headerXClientId, fmt.Sprintf("%v", at.ClientId))
-
+	request.Header.Add(headerXCallerId, fmt.Sprintf("%v", at.UserId))
 	return nil
 }
 
@@ -97,31 +91,29 @@ func cleanRequest(request *http.Request) {
 	if request == nil {
 		return
 	}
-
-	request.Header.Del(headerXCallerId)
+	request.Header.Del(headerXClientId)
 	request.Header.Del(headerXCallerId)
 }
 
-func getAccessToken(accessTokenId string) (*accessToken, *errors.RestErr) {
-	fmt.Print("Here")
-	response := oauthRestClient.Get(fmt.Sprintf("oauth/access_token/%s", accessTokenId))
-
+func getAccessToken(accessTokenId string) (*accessToken, rest_errors.RestErr) {
+	response := oauthRestClient.Get(fmt.Sprintf("/oauth/access_token/%s", accessTokenId))
 	if response == nil || response.Response == nil {
-		return nil, errors.NewInternalServerError("Here invalid restclient response when trying to access token")
+		return nil, rest_errors.NewInternalServerError("invalid restclient response when trying to get access token",
+			errors.New("network timeout"))
 	}
 
 	if response.StatusCode > 299 {
-		var restErr errors.RestErr
-		if err := json.Unmarshal(response.Bytes(), &restErr); err != nil {
-			fmt.Println("here")
-			return nil, errors.NewInternalServerError("here 2 invalid restclient response when trying to access token")
+		restErr, err := rest_errors.NewRestErrorFromBytes(response.Bytes())
+		if err != nil {
+			return nil, rest_errors.NewInternalServerError("invalid error interface when trying to get access token", err)
 		}
-		return nil, &restErr
+		return nil, restErr
 	}
 
 	var at accessToken
 	if err := json.Unmarshal(response.Bytes(), &at); err != nil {
-		return nil, errors.NewInternalServerError("error when trying to unmarshal access token response")
+		return nil, rest_errors.NewInternalServerError("error when trying to unmarshal access token response",
+			errors.New("error processing json"))
 	}
 	return &at, nil
 }
